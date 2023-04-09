ea954fdc927b078af7d2576ad0cf8928f9174d35
Luis020-hub <guhmonteiro10@hotmail.com>
2023-04-08 23:43:25 -0300

    08/04/2023
    - Criada a tela de carregamento.
    - Instalando a dependência percent_indicator para a tela de carregamento.

---
+++ lib/pages/loading_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+
+class LoadingPage extends StatelessWidget {
+  const LoadingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: CircularPercentIndicator(
+                animation: true,
+                animationDuration: 900,
+                radius: 70.0,
+                lineWidth: 8.0,
+                percent: 1.0,
+                center: const Text(
+                  "100%",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+                progressColor: Colors.white,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
