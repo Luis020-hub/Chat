32eebdcecfa747acc66c33967335216dc4b3bbb5
Luis020-hub <guhmonteiro10@hotmail.com>
2023-04-09 21:48:52 -0300

    09/04/2023
    - Convertendo para stateful.
    - Melhorando a autenticação do formulário.

--- lib/pages/auth_form.dart
+++ lib/pages/auth_form.dart
@@ -1,7 +1,20 @@
+import 'package:chat/models/auth_form_data.dart';
 import 'package:flutter/material.dart';
 
-class AuthForm extends StatelessWidget {
+class AuthForm extends StatefulWidget {
   const AuthForm({super.key});
+
+  @override
+  State<AuthForm> createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _authFormData = AuthFormData();
+
+  void _submit() {
+    _formKey.currentState?.validate();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,15 +23,26 @@ class AuthForm extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Form(
+          key: _formKey,
           child: Column(
             children: [
+              if (_authFormData.isSignup)
+                TextFormField(
+                  key: const ValueKey('Name'),
+                  initialValue: _authFormData.name,
+                  onChanged: (name) => _authFormData.name = name,
+                  decoration: const InputDecoration(labelText: 'Name'),
+                ),
               TextFormField(
-                decoration: const InputDecoration(labelText: 'Name'),
-              ),
-              TextFormField(
+                key: const ValueKey('E-mail'),
+                initialValue: _authFormData.email,
+                onChanged: (email) => _authFormData.email = email,
                 decoration: const InputDecoration(labelText: 'E-mail'),
               ),
               TextFormField(
+                key: const ValueKey('Password'),
+                initialValue: _authFormData.password,
+                onChanged: (password) => _authFormData.password = password,
                 obscureText: true,
                 decoration: const InputDecoration(labelText: 'Password'),
               ),
@@ -27,12 +51,20 @@ class AuthForm extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   TextButton(
-                    onPressed: () {},
-                    child: const Text('Create a new account'),
+                    child: Text(
+                      _authFormData.isLogin
+                          ? 'Create a new account'
+                          : 'Already have an account?',
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _authFormData.toggleAuthMode();
+                      });
+                    },
                   ),
                   ElevatedButton(
-                    onPressed: () {},
-                    child: const Text('Login'),
+                    onPressed: _submit,
+                    child: Text(_authFormData.isLogin ? 'Login' : 'Sign up'),
                   ),
                 ],
               ),
