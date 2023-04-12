4b1a30f31d2c1d9f03a09a76788d9cf9984dabf2
Luis020-hub <guhmonteiro10@hotmail.com>
2023-04-11 21:03:39 -0300

    11/04/2023
    - Comunicação entre as classes.
    - Mudando para stateful.
    - Adicionando o loading ao logar.

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,17 +1,58 @@
+import 'package:chat/models/auth_form_data.dart';
 import 'package:chat/pages/auth_form.dart';
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 
-class AuthPage extends StatelessWidget {
+class AuthPage extends StatefulWidget {
   const AuthPage({super.key});
+
+  @override
+  State<AuthPage> createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  bool _isLoading = false;
+
+  void _handleSubmit(AuthFormData formData) {
+    setState(() => _isLoading = true);
+ // setState(() => _isLoading = false);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.primary,
-      body: const Center(
-        child: SingleChildScrollView(
-          child: AuthForm(),
-        ),
+      body: Stack(
+        children: [
+          Center(
+            child: SingleChildScrollView(
+              child: AuthForm(onSubmit: _handleSubmit),
+            ),
+          ),
+          if (_isLoading == true)
+            Container(
+              decoration: const BoxDecoration(
+                color: Color.fromRGBO(0, 0, 0, 0.5),
+              ),
+              child: Center(
+                child: CircularPercentIndicator(
+                  animation: true,
+                  animationDuration: 900,
+                  radius: 70.0,
+                  lineWidth: 8.0,
+                  percent: 1.0,
+                  center: const Text(
+                    "100%",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                  progressColor: Colors.white,
+                ),
+              ),
+            ),
+        ],
       ),
     );
   }
