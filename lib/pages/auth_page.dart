1a1cf602ae826d5eef57ea2bb6953e30ecd7b0c8
Luis020-hub <guhmonteiro10@hotmail.com>
2023-04-17 22:43:33 -0300

    17/04/2023
    - Usando factory.
    - Substituindo o AuthMockService por AuthService.

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,5 +1,5 @@
 import 'package:chat/core/models/auth_form_data.dart';
-import 'package:chat/core/services/auth/auth_mock_service.dart';
+import 'package:chat/core/services/auth/auth_service.dart';
 import 'package:chat/pages/auth_form.dart';
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
@@ -19,12 +19,12 @@ class _AuthPageState extends State<AuthPage> {
       setState(() => _isLoading = true);
 
       if (formData.isLogin) {
-        await AuthMockService().login(
+        await AuthService().login(
           formData.email,
           formData.password,
         );
       } else {
-        await AuthMockService().signup(
+        await AuthService().signup(
           formData.name,
           formData.email,
           formData.password,
