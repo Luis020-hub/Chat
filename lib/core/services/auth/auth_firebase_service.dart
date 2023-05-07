25f5855f0d9bf5ee91f5d5fa55060e84b39c2239
Luis020-hub <guhmonteiro10@hotmail.com>
2023-05-07 17:58:07 -0300

    07/05/2023
    - Salvando os usuários no Cloud Firestore.

--- lib/core/services/auth/auth_firebase_service.dart
+++ lib/core/services/auth/auth_firebase_service.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'dart:async';
 import 'package:chat/core/models/chat_user.dart';
 import 'package:chat/core/services/auth/auth_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
@@ -40,13 +41,16 @@ class AuthFirebaseService implements AuthService {
 
     if (credential.user == null) return;
 
-    // 1. Upload da foto do usuário
+    // 1. Upload da foto do usuário.
     final imageName = '${credential.user!.uid}.jpg';
     final imageURL = await _uploadUserImage(image, imageName);
 
-    // 2. atualizar os atributos do usuário
-    credential.user?.updateDisplayName(name);
-    credential.user?.updatePhotoURL(imageURL);
+    // 2. atualizar os atributos do usuário.
+    await credential.user?.updateDisplayName(name);
+    await credential.user?.updatePhotoURL(imageURL);
+
+    // 3. Salvando o usuario no banco de dados.
+    await _saveChatUser(_toChatUser(credential.user!, imageURL));
   }
 
   @override
@@ -71,12 +75,23 @@ class AuthFirebaseService implements AuthService {
     return await imageRef.getDownloadURL();
   }
 
-  static ChatUser _toChatUser(User user) {
+  Future<void> _saveChatUser(ChatUser user) async {
+    final store = FirebaseFirestore.instance;
+    final docRef = store.collection('users').doc(user.id);
+
+    await docRef.set({
+      'name': user.name,
+      'email': user.email,
+      'imageUrl': user.imageUrl,
+    });
+  }
+
+  static ChatUser _toChatUser(User user, [String? imageUrl]) {
     return ChatUser(
       id: user.uid,
       name: user.displayName ?? user.email!.split('@')[0],
       email: user.email!,
-      imageUrl: user.photoURL ?? 'assets/images/avatar.png',
+      imageUrl: imageUrl ?? user.photoURL ?? 'assets/images/avatar.png',
     );
   }
 }
